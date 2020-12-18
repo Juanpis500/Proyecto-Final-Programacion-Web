b4c58bf52444f5f6f7378a43dd9c4735ca52265a
Juanpis <juanpis65@hotmail.com>
2020-12-18 14:52:53 -0600

    Esturctura basica del proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ISChop</title>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <Section>
+        
+    </Section>
+    <footer>
+        
+    </footer>
+</body>
+</html>
