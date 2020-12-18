e28775c30e1cf53294498976f357b4952f12ae6f
veronicaromo <veronicalizethromo@gmail.com>
2020-12-18 15:28:31 -0600

    Menu

--- index.php
+++ index.php
@@ -6,7 +6,18 @@
 </head>
 <body>
     <header>
-        
+        <div class="wrapper">
+           <div><img src="images/logo.jpg" alt="ischop" width="100px" class="logo"></div>
+            <nav class="navmenu">
+                    <a href="#index.html"><i class="fas fa-home"></i></a>
+                    <a href="#">Catálogo 1</a>
+                    <a href="#">Catálogo 2</a>
+                    <a href="#">Catálogo 3</a>
+                    <a href="#">Sobre nosotros</a>
+                    <a href="#">Preguntas frecuentes</a>
+                    <a href="#">Contáctanos</a>
+            </nav>
+        </div>
     </header>
     <Section>
         
