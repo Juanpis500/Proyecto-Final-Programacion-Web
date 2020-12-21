a7dda748d711ceda2f5785bb0bfbf88879825d7d
unknown <abilopezrivera2000@gmail.com>
2020-12-20 22:08:11 -0600

    Página de Ayuda

---
+++ ayuda.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ayuda</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+    <link rel="stylesheet" href="CSS/estilos.css">
+    <link rel="stylesheet" href="CSS/estilosAyuda.css">
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/3.0.2/normalize.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    
+    <script src="https://kit.fontawesome.com/62034752b0.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div>
+           <nav class="carrito">
+               <a href="#"><i class="fas fa-shopping-cart"></i></a>
+               <a href="#">Login</a>
+           </nav>
+           <br><br>
+           
+           <!-------------------- MENU -------------------->
+            <nav  class="menu">
+               <div class="contenedor">
+                <ul>
+                    <li><a href="index.php"><i class="fas fa-home"></i></a></li>
+                    <li><a href="#">Tienda</a></li>
+                    <li><a href="acercade.php">Acerca de</a></li>
+                    <li><a href="ayuda.php">Ayuda</a></li> 
+                    <li><a href="contacto.php">Contáctanos</a></li>
+                </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <br><br>
+    
+    <img src="images/FAQ.png" alt="Preguntas Frecuentes" width="900px" class="centrar-faq">
+    
+    <br><br>
+    <details>
+        <summary>Después de hacer mi compra, ¿en cuántos días recibiré mi pedido?</summary>
+        <p>Depende de tu lugar de residencia, el pedido tardará en llegar de 3 a 7 días hábiles.</p>
+    </details>
+    <br>
+    
+    <details>
+        <summary>Tengo un cupón de descuento, ¿cómo lo aplico?</summary>
+        <p>Si cuentas con un código de descuento por alguna promoción, debes ingresarlo en la página de carrito de compra en la opción "Aplicar código de descuento". </p>
+    </details>
+    <br>
+    
+    <details>
+        <summary>¿Cuáles son los métodos de pago?</summary>
+        <p>En ISChop manejamos dos maneras de pago: por medio de <strong>tarjeta</strong> (de crédito y débito) y haciendo uso del servicio de <strong>Oxxo Pay</strong>.</p>
+    </details>
+    <br>
+    
+    <details>
+        <summary>¿Cómo puedo pagar con tarjeta de crédito o débito?</summary>
+        <p>Pasos: <br> 1.- Elije Tarjeta como método de pago. <br>2.- Llena los datos  que se te piden y realiza el pago <br>
+        3.-Espera en tu correo una verifiación de pago. </p>
+    </details>
+    <br>
+    
+    <details>
+        <summary>¿Cómo pagar en Oxxo?</summary>
+        <p>Este método de pago es <strong>únicamente en efectivo</strong> y a través de tiendas Oxxo, toma en cuenta que el total de tu compra <strong>no debe rebasar los $10,000MXN.</strong></p>
+        <p>1.- Elige <strong>Oxxo</strong> como método de pago. <br>2.- Recibe la confirmación de tu pedido y una clave única para realizar el pago en la sucursal Oxxo. <br>3.- Visita el Oxxo y dile al cajero que <strong>deseas realizar un pago de Oxxo pay</strong>. <br>
+        4.- Dictale la <strong>referencia de pago</strong> que recibiste anteriormente. <br>5.- Espera en tu correo una verificación de pago.</p>
+    </details>
+    
+    <br><br>
+    <h3>Si tu pregunta no ha sido agregada a esta sección, recuerda que siempre puedes contactarnos para aclarar tus dudas sobre los servicios y productos que te ofrecemos en el apartado <a href="contacto.php">Contáctanos</a>.</h3>
+    <br><br>
+    <footer>
+        <nav class="pie">
+            <div>
+                <h4>Acerca de ISChop</h4>
+                <a href="#">¿Quiénes somos?</a>
+            </div>
+            
+            <div>
+                <h4>Servicio al cliente</h4>
+                <a href="#">Preguntas Frecuentes</a><br>
+                <a href="contacto.php">Contáctanos</a>
+            </div>
+            
+            <div>
+                <h4>Visítanos en redes sociales</h4>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram-square"></i></a>
+                <a href="#"><i class="fab fa-twitter-square"></i></a>
+            </div>
+
+        </nav>
+        <div id="derechos">
+            <p>Todos los derechos resevados <i class="far fa-copyright"></i> 2020 ISChop utiliza cookies para almacenar los datos de la cuenta.</p>
+        </div>
+    </footer>
+</body>
+</html>
