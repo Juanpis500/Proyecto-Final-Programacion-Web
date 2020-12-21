4acc9c303db5a18b9e24e2c4c63435ca1549c6d7
Juanpis <juanpis65@hotmail.com>
2020-12-21 01:13:18 -0600

    Proceso de la pagina del administrador

---
+++ pag_admin_1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <title>Administrar Pagina</title>
+    <link rel="stylesheet" href="CSS/estilos.css">
+    <script src="https://kit.fontawesome.com/62034752b0.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://csshake.surge.sh/csshake.min.css">
+    <link rel="stylesheet"    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> 
+    <link rel="stylesheet" href="CSS/estilos_Pag_admin.css">
+</head>
+<body>
+    <header>
+        <div>
+           <nav class="carrito">
+               <a href="#">Login</a>
+           </nav>
+           <br><br>
+           <!-------------------- MENU -------------------->
+            <nav  class="menu">
+               <div class="contenedor">
+                <ul>
+                    <li><a href="index.php"><i class="fas fa-home"></i></a></li>
+                    <li><a href="#">Tienda</a></li>
+                </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <section id="sec_admin">
+       <br>
+        <h3 style="text-align: center;"> Administrar pagina</h3>
+        <br>
+        <div id="config_admin">
+            <div id="Menus_admin">
+                <ul>
+                    <li>Configuracion</li><br>
+                    <li><a href="">Agregar Nuevos profuctos</a></li>
+                    <li><a href="">Administrar productos existentes</a></li>
+                    <li><a href="">Estadisticas de la pagina</a></li>
+                </ul>
+            </div>
+            <div id="sec2_admin">
+                <p>Agregar nuevo productos:</p><br><br><br>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <footer>
+        <div id="derechos">
+            <p>Todos los derechos resevados <i class="far fa-copyright"></i> 2020 ISChop utiliza cookies para almacenar los datos de la cuenta.</p>
+        </div>
+    </footer>
+</body>
+</html>
